84277f3198988fea0079311638825047aec8f1a8
eliezer <eliezer884@gmail.com>
2024-10-11 00:12:02 +0300

    tests

--- web-socket-reactive/src/main/java/com/reuven/websocketreactive/services/MessageHandler.java
+++ web-socket-reactive/src/main/java/com/reuven/websocketreactive/services/MessageHandler.java
@@ -24,7 +24,8 @@ public class MessageHandler implements WebSocketHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(MessageHandler.class);
 
-    private static final String DELAY_SERVICE_URI = "http://{%s}:8081/api/delay/{%s}";
+//    private static final String DELAY_SERVICE_URI = "http://{%s}:8081/api/delay/{%s}";
+    private static final String DELAY_SERVICE_URI = "http://localhost:8081/api/delay/{%s}";
 
     //    public static final Duration WS_OPEN_CONNECTION_DURATION = Duration.ofMinutes(10);  // timeout for disconnection socket without actions
     private static final Duration FIXED_DELAY_ON_RETRY = Duration.ofSeconds(1);
@@ -75,7 +76,8 @@ public class MessageHandler implements WebSocketHandler {
 //                            ))));
 //                })
                 .flatMap(requestMessage -> webClient.get()
-                        .uri(DELAY_SERVICE_URI, delayServiceHost, session.getId())
+//                        .uri(DELAY_SERVICE_URI, delayServiceHost, session.getId())
+                        .uri(DELAY_SERVICE_URI, session.getId())
                         .retrieve()
                         .bodyToMono(String.class)
                         .onErrorResume(sendError -> {
