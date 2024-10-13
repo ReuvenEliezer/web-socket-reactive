40f294feb1edcc318d72772b14b853376a76b540
eliezer.reuven@nice.com <144931323+eliezer-nice@users.noreply.github.com>
2024-10-13 21:28:26 +0300

    add traceId and SpanId (#3)

--- delay-service-reactive/src/main/java/com/reuven/delayservicereactive/DelayServiceReactiveApplication.java
+++ delay-service-reactive/src/main/java/com/reuven/delayservicereactive/DelayServiceReactiveApplication.java
@@ -2,11 +2,13 @@ package com.reuven.delayservicereactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class DelayServiceReactiveApplication {
 
     public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation(); //for tracing log in reactive
         SpringApplication.run(DelayServiceReactiveApplication.class, args);
     }
 
--- web-socket-reactive/src/main/java/com/reuven/websocketreactive/WebSocketReactiveApplication.java
+++ web-socket-reactive/src/main/java/com/reuven/websocketreactive/WebSocketReactiveApplication.java
@@ -3,6 +3,7 @@ package com.reuven.websocketreactive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import reactor.core.publisher.Hooks;
 import reactor.tools.agent.ReactorDebugAgent;
 
 @ComponentScan(basePackages = {
@@ -13,6 +14,7 @@ import reactor.tools.agent.ReactorDebugAgent;
 public class WebSocketReactiveApplication {
 
     public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation(); //for tracing log in reactive
         ReactorDebugAgent.init();
         SpringApplication.run(WebSocketReactiveApplication.class, args);
     }
